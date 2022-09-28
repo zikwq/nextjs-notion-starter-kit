42ecbeee3b6ae3c678220646c635b80badc8d26e
zikwq <103549378+zikwq@users.noreply.github.com>
2022-09-28 19:25:28 +0800

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css'
 export const GitHubShareButton: React.FC = () => {
   return (
     <a
-      href='https://github.com/chusight'
+      href='https://github.com/zikwq'
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
